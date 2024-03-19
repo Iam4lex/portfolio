798b4bf026c46bb2768e8fd5b9bbd10df1ab5320
Alex Mwangi <mungaialexm@gmail.com>
2024-03-19 16:54:44 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,7 @@
       return document.querySelector(el)
     }
   }
+  
 
   /**
    * Easy event listener function
--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,45 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Replace these values with your actual database credentials
+$servername = "localhost";
+$username = "username";
+$password = "";
+$database = "FirstDataBase";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+// Prepare data for insertion
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Prepare SQL query
+$sql = "INSERT INTO form_submissions (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
 
-  echo $contact->send();
+// Execute query
+if ($conn->query($sql) === TRUE) {
+    // Email sending code here (you can use mail() function as shown in the previous example)
+    // Replace 'alexmwaangimungai254@gmail.com' with your actual receiving email address
+    $receiving_email_address = 'alexmwaangimungai254@gmail.com';
+    $mail_sent = mail($receiving_email_address, $subject, $message);
+    
+    if ($mail_sent) {
+        echo "1"; // Success
+    } else {
+        echo "0"; // Error
+    }
+} else {
+    echo "0"; // Error
+}
+
+// Close connection
+$conn->close();
+
 ?>
